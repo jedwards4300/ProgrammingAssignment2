46ce4b118efe1ea2d1263402d06ade237def6993
Justin Edwards <jedwards4300@gmail.com>
2014-04-24 18:55:11 -0400

    Initial Create

---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = numeric()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
